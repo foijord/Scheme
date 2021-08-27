d0c4c7a70c89b2c5a957618ff2c6317e4e0acf3f
Frode Oijord <foijord@gmail.com>
2021-08-27 12:17:42 +0200

    initial commit

---
+++ Repl.cpp
@@ -0,0 +1,72 @@
+
+#include <Scheme.h>
+
+#include <string>
+#include <vector>
+#include <ranges>
+#include <iostream>
+#include <algorithm>
+
+scm::env_ptr env = scm::global_env();
+
+bool test(const std::string& exp, const std::string& expected_result)
+{
+	return [=] {
+		std::string result = scm::print(scm::eval(scm::read(exp), env));
+		std::cout << exp << " => " << result << " ";
+		bool passed = (expected_result == result);
+		std::cout << (passed ? "(Pass)" : "(Fail)") << std::endl;
+		return passed;
+	}();
+}
+
+int main(int, char* [])
+{
+	std::vector<bool> tests{
+		test("(quote ())", "()"),
+		test("(begin (define a 1) (+ 1 2 3))", "6"),
+		test("a", "1"),
+		test("(quote (testing 1 (2) -3.14e+159))", "(testing 1 (2) -3.14e+159)"),
+		test("(+ 2 2)", "4"),
+		test("(+ (* 2 100) (* 1 10))", "210"),
+		test("(if (> 6 5) (+ 1 1) (+ 2 2))", "2"),
+		test("(if (< 6 5) (+ 1 1) (+ 2 2))", "4"),
+		test("(define x 3)", "3"),
+		test("x", "3"),
+		test("(+ x x)", "6"),
+		test("((lambda (x) (+ x x)) 5)", "10"),
+		test("(define twice (lambda (x) (* 2 x)))", "struct scm::Function"),
+		test("(twice 5)", "10"),
+		test("(define compose (lambda (f g) (lambda (x) (f (g x)))))", "struct scm::Function"),
+		test("((compose list twice) 5)", "(10)"),
+		test("(define repeat (lambda (f) (compose f f)))", "struct scm::Function"),
+		test("((repeat twice) 5)", "20"),
+		test("((repeat (repeat twice)) 5)", "80"),
+		test("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))", "struct scm::Function"),
+		test("(fact 3)", "6"),
+		test("(fact 50)", "3.04141e+64"),
+		test("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))", "struct scm::Function"),
+		test("(list (abs -3) (abs 0) (abs 3))", "(3 0 3)"),
+	};
+
+	std::cout << tests.size() << " tests executed." << std::endl;
+	std::cout << std::ranges::count(tests, false) << " tests failed." << std::endl;
+	std::cout << std::ranges::count(tests, true) << +" tests passed." << std::endl;
+
+	std::cout << "Scheme REPL" << std::endl;
+
+	while (true) {
+		try {
+			std::cout << "> ";
+			std::string input;
+			std::getline(std::cin, input);
+			std::cout << scm::eval(scm::read(input), env) << std::endl;
+		}
+		catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ Scheme.h
@@ -0,0 +1,388 @@
+#pragma once
+
+#include <any>
+#include <regex>
+#include <vector>
+#include <memory>
+#include <sstream>
+#include <fstream>
+#include <numeric>
+#include <variant>
+#include <numbers>
+#include <iostream>
+#include <typeinfo>
+#include <algorithm>
+#include <functional>
+#include <unordered_map>
+
+#include <boost/fusion/adapted.hpp>
+#include <boost/spirit/home/x3.hpp>
+
+namespace scm {
+
+	typedef std::vector<std::any> List;
+	typedef std::shared_ptr<List> lst_ptr;
+	typedef std::function<std::any(const List& args)> fun_ptr;
+
+	class Env;
+	typedef std::shared_ptr<Env> env_ptr;
+
+	typedef bool Boolean;
+	typedef double Number;
+	typedef std::string String;
+
+	template <typename Container>
+	std::any read(const Container& input);
+
+	struct Symbol : public std::string {
+		Symbol() = default;
+		explicit Symbol(const String& s) : std::string(s) {}
+	};
+
+	struct If {
+		std::any test, conseq, alt;
+	};
+
+	struct Quote {
+		std::any exp;
+	};
+
+	struct Define {
+		Symbol sym;
+		std::any exp;
+	};
+
+	struct Lambda {
+		std::any parms, body;
+	};
+
+	struct Begin {
+		lst_ptr exps;
+	};
+
+	struct Function {
+		std::any parms, body;
+		env_ptr env;
+	};
+
+	struct Import {
+		String code;
+	};
+
+
+	class Env {
+	public:
+		Env() = default;
+		~Env() = default;
+
+		explicit Env(std::unordered_map<std::string, std::any> inner)
+			: inner(inner)
+		{}
+
+		Env(const std::any& parm, const List& args, env_ptr outer)
+			: outer(std::move(outer))
+		{
+			if (parm.type() == typeid(lst_ptr)) {
+				auto parms = std::any_cast<lst_ptr>(parm);
+				for (size_t i = 0; i < parms->size(); i++) {
+					auto sym = std::any_cast<Symbol>((*parms)[i]);
+					this->inner[sym] = args[i];
+				}
+			}
+			else {
+				auto sym = std::any_cast<Symbol>(parm);
+				this->inner[sym] = args;
+			}
+		}
+
+		std::any get(Symbol sym)
+		{
+			if (this->inner.contains(sym)) {
+				return this->inner.at(sym);
+			}
+			if (this->outer) {
+				return this->outer->get(sym);
+			}
+			throw std::runtime_error("undefined symbol: " + sym);
+		}
+
+		std::unordered_map<std::string, std::any> inner;
+		env_ptr outer{ nullptr };
+	};
+
+	template <typename Op>
+	Number accumulate(const List& lst)
+	{
+		std::vector<Number> args(lst.size());
+		std::transform(lst.begin(), lst.end(), args.begin(), [](std::any exp) { return std::any_cast<Number>(exp); });
+		return std::accumulate(next(args.begin()), args.end(), args.front(), Op());
+	}
+
+	env_ptr global_env()
+	{
+		return std::make_shared<Env>(
+			std::unordered_map<std::string, std::any>{
+				{ Symbol("pi"), Number(std::numbers::pi) },
+				{ Symbol("+"), fun_ptr(accumulate<std::plus<Number>>) },
+				{ Symbol("-"), fun_ptr(accumulate<std::minus<Number>>) },
+				{ Symbol("/"), fun_ptr(accumulate<std::divides<Number>>) },
+				{ Symbol("*"), fun_ptr(accumulate<std::multiplies<Number>>) },
+				{ Symbol(">"), fun_ptr([](const List& lst) { return Boolean(std::any_cast<Number>(lst[0]) > std::any_cast<Number>(lst[1])); }) },
+				{ Symbol("<"), fun_ptr([](const List& lst) { return Boolean(std::any_cast<Number>(lst[0]) < std::any_cast<Number>(lst[1])); }) },
+				{ Symbol("<="), fun_ptr([](const List& lst) { return Boolean(std::any_cast<Number>(lst[0]) <= std::any_cast<Number>(lst[1])); }) },
+				{ Symbol(">="), fun_ptr([](const List& lst) { return Boolean(std::any_cast<Number>(lst[0]) >= std::any_cast<Number>(lst[1])); }) },
+				{ Symbol("="), fun_ptr([](const List& lst) { return Boolean(std::any_cast<Number>(lst[0]) == std::any_cast<Number>(lst[1])); }) },
+				{ Symbol("car"), fun_ptr([](const List& lst) { return std::any_cast<lst_ptr>(lst.front())->front(); }) },
+				{ Symbol("cdr"), fun_ptr([](const List& lst) { auto l = std::any_cast<lst_ptr>(lst.front()); return std::make_shared<List>(next(l->begin()), l->end()); }) },
+				{ Symbol("list"), fun_ptr([](const List& lst) { return std::make_shared<List>(lst); }) },
+				{ Symbol("length"), fun_ptr([](const List& lst) { return static_cast<Number>(std::any_cast<lst_ptr>(lst.front())->size()); }) }
+		});
+	}
+
+	std::ostream& operator<<(std::ostream& os, const std::any exp)
+	{
+		if (exp.type() == typeid(lst_ptr)) {
+			auto list = std::any_cast<lst_ptr>(exp);
+			os << "(";
+			if (!list->empty()) {
+				for (size_t i = 0; i < list->size(); i++) {
+					os << list->at(i);
+					if (i != list->size() - 1) {
+						os << " ";
+					}
+				}
+			}
+			return os << ")";
+		}
+		if (exp.type() == typeid(Number)) {
+			return os << std::any_cast<Number>(exp);
+		}
+		if (exp.type() == typeid(Symbol)) {
+			return os << std::any_cast<Symbol>(exp);
+		}
+		if (exp.type() == typeid(String)) {
+			return os << std::any_cast<String>(exp);
+		}
+		if (exp.type() == typeid(Boolean)) {
+			return os << (std::any_cast<Boolean>(exp) ? "true" : "false");
+		}
+		return os << exp.type().name();
+	};
+
+	std::string print(const std::any exp)
+	{
+		std::stringstream ss;
+		ss << exp;
+		return ss.str();
+	}
+
+	std::any eval(std::any exp, env_ptr env)
+	{
+		while (true) {
+			if (exp.type() == typeid(Number) ||
+				exp.type() == typeid(String) ||
+				exp.type() == typeid(Boolean)) {
+				return exp;
+			}
+			if (exp.type() == typeid(Symbol)) {
+				auto symbol = std::any_cast<Symbol>(exp);
+				return env->get(symbol);
+			}
+			if (exp.type() == typeid(Define)) {
+				auto define = std::any_cast<Define>(exp);
+				return env->inner[define.sym] = eval(define.exp, env);
+			}
+			if (exp.type() == typeid(Lambda)) {
+				auto lambda = std::any_cast<Lambda>(exp);
+				return Function{ lambda.parms, lambda.body, env };
+			}
+			if (exp.type() == typeid(Quote)) {
+				auto quote = std::any_cast<Quote>(exp);
+				return quote.exp;
+			}
+			if (exp.type() == typeid(Import)) {
+				auto import = std::any_cast<Import>(exp);
+				std::any exp = read(import.code);
+				return eval(exp, env);
+			}
+			if (exp.type() == typeid(If)) {
+				auto if_ = std::any_cast<If>(exp);
+				exp = std::any_cast<Boolean>(eval(if_.test, env)) ? if_.conseq : if_.alt;
+			}
+			else if (exp.type() == typeid(Begin)) {
+				auto begin = std::any_cast<Begin>(exp);
+				std::transform(begin.exps->begin(), std::prev(begin.exps->end()), begin.exps->begin(),
+					std::bind(eval, std::placeholders::_1, env));
+				exp = begin.exps->back();
+			}
+			else {
+				auto list = std::any_cast<lst_ptr>(exp);
+				auto call = List(list->size());
+
+				std::transform(list->begin(), list->end(), call.begin(),
+					std::bind(eval, std::placeholders::_1, env));
+
+				auto func = call.front();
+				auto args = List(std::next(call.begin()), call.end());
+
+				if (func.type() == typeid(Function)) {
+					auto function = std::any_cast<Function>(func);
+					exp = function.body;
+					env = std::make_shared<Env>(function.parms, args, function.env);
+				}
+				else if (func.type() == typeid(fun_ptr)) {
+					auto function = std::any_cast<fun_ptr>(func);
+					return function(args);
+				}
+				else {
+					throw std::runtime_error("undefined function: " + print(func));
+				}
+			}
+		}
+	}
+
+	typedef std::variant<Number, String, Symbol, Boolean, std::vector<struct value>> value_t;
+
+	struct value : value_t {
+		using base_type = value_t;
+		using base_type::variant;
+	};
+
+	std::any expand(value const& v);
+
+	struct visitor {
+		template <typename T>
+		std::any operator()(const T& v) const { return v; }
+
+		template <typename T>
+		std::any operator()(const std::vector<T>& v) const
+		{
+			if (v.empty()) {
+				return std::make_shared<List>();
+			}
+
+			List list(v.size());
+			std::transform(v.begin(), v.end(), list.begin(), expand);
+
+			if (list[0].type() == typeid(Symbol)) {
+				auto token = std::any_cast<Symbol>(list[0]);
+
+				if (token == "<" || token == ">" || token == "<=" || token == ">=" || token == "==") {
+					if (list.size() != 3) {
+						throw std::invalid_argument("wrong number of arguments to " + token);
+					}
+				}
+				if (token == "quote") {
+					if (list.size() != 2) {
+						throw std::invalid_argument("wrong number of arguments to quote");
+					}
+					return Quote{ .exp = list[1] };
+				}
+				if (token == "if") {
+					if (list.size() != 4) {
+						throw std::invalid_argument("wrong number of arguments to if");
+					}
+					return If{ .test = list[1], .conseq = list[2], .alt = list[3] };
+				}
+				if (token == "lambda") {
+					if (list.size() != 3) {
+						throw std::invalid_argument("wrong Number of arguments to lambda");
+					}
+					return Lambda{ .parms = list[1], .body = list[2] };
+				}
+				if (token == "begin") {
+					if (list.size() < 2) {
+						throw std::invalid_argument("wrong Number of arguments to begin");
+					}
+					return Begin{ .exps = std::make_shared<List>(std::next(list.begin()), list.end()) };
+				}
+				if (token == "define") {
+					if (list.size() < 3 || list.size() > 4) {
+						throw std::invalid_argument("wrong number of arguments to define");
+					}
+					if (list[1].type() != typeid(Symbol)) {
+						throw std::invalid_argument("first argument to define must be a Symbol");
+					}
+					if (list.size() == 3) {
+						return Define{ .sym = std::any_cast<Symbol>(list[1]), .exp = list[2] };
+					}
+					return Define{ .sym = std::any_cast<Symbol>(list[1]), .exp = Lambda{ list[2], list[3] } };
+				}
+				if (token == "import") {
+					if (list.size() != 2) {
+						throw std::invalid_argument("wrong number of arguments to import");
+					}
+					if (list[1].type() != typeid(String)) {
+						throw std::invalid_argument("Argument to import must be a String");
+					}
+					String filename = std::any_cast<String>(list[1]);
+					std::ifstream stream(filename, std::ios::in);
+					return Import{ std::string{
+						std::istreambuf_iterator<char>(stream),
+						std::istreambuf_iterator<char>()
+					} };
+				}
+			}
+			return std::make_shared<List>(list);
+		}
+	};
+
+	std::any expand(value const& v)
+	{
+		visitor expander;
+		return std::visit(expander, v);
+	}
+
+	namespace parser {
+		namespace x3 = boost::spirit::x3;
+		using x3::char_;
+		using x3::lexeme;
+		using x3::double_;
+
+		x3::rule<struct symbol_class, Symbol> symbol_ = "symbol";
+		x3::rule<struct number_class, Number> number_ = "number";
+		x3::rule<struct string_class, String> string_ = "string";
+		x3::rule<struct value_class, value> value_ = "value";
+		x3::rule<struct list_class, std::vector<value> > list_ = "list";
+		x3::rule<struct multi_string_class, String> multi_string_ = "multi_string";
+
+		struct bool_table : x3::symbols<bool> {
+			bool_table() {
+				add("true", true) ("false", false);
+			}
+		} const boolean_;
+
+		const auto number__def = double_;
+		const auto string__def = lexeme['"' >> *(char_ - '"') >> '"'];
+		const auto multi_string__def = lexeme["[[" >> *(char_ - "]]") >> "]]"];
+		const auto symbol__def = lexeme[+(char_("A-Za-z") | char_("0-9") | char_('_') | char_('-') | char_("+*/%~&|^!=<>?"))];
+		const auto list__def = '(' >> *value_ >> ')';
+
+		const auto value__def
+			= number_
+			| string_
+			| boolean_
+			| multi_string_
+			| symbol_
+			| list_;
+
+		BOOST_SPIRIT_DEFINE(value_, number_, string_, multi_string_, symbol_, list_)
+
+			const auto entry_point = x3::skip(x3::space)[value_];
+	}
+
+
+	template <typename Container>
+	std::any read(const Container& input)
+	{
+		value val;
+		visitor expander;
+
+		if (parse(std::begin(input), std::end(input), parser::entry_point, val)) {
+			return std::visit(expander, val);
+		}
+		throw std::runtime_error("Parse failed, remaining input: " + std::string(std::begin(input), std::end(input)));
+	}
+}
+
+using scm::operator<<;
